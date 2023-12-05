2b9cfe9cd5c5d8329f7b1cb081e41cea688017ae
Lark Mendoza <119717119+mtn-lark@users.noreply.github.com>
2023-12-04 22:13:56 -0800

    Day 3 Part 2

--- Day3/gear_ratios.py
+++ Day3/gear_ratios.py
@@ -8,6 +8,18 @@ lines: List[str] = []
 line_length: int = 0
 num_lines: int = 0
 
+# Potential gear dictionary
+# Keys are formatted row.col
+potential_gears: Dict[str, List[int]] = {}
+
+
+def add_potential_gear(row_index: int, col_index: int, num: int) -> None:
+    key = f"{row_index}.{col_index}"
+    if key in potential_gears:
+        potential_gears[key].append(num)
+    else:
+        potential_gears[key] = [num]
+
 
 def is_symbol(char: str) -> bool:
     if not char.isdigit() and char != "." and char != "\n":
@@ -35,10 +47,55 @@ def is_part(row_index: int, start_index: int, end_index: int) -> bool:
                     # Get character
                     char = lines[r][i]
                     if is_symbol(char):
+                        if char == "*":
+                            add_potential_gear(r, i, int(number_string))
                         return True
 
     # Symbol not found in scan
     return False
+
+
+def sum_part_numbers() -> int:
+    running_sum: int = 0
+    row_index: int = 0
+    for line in lines:
+        # Start/end indices are -1 if currently reading a non-digit.
+        current_index: int = 0
+        start_index: int = -1
+        end_index: int = -1
+
+        # Read numbers in line by reading each character
+        for char in line:
+            if char.isdigit():
+                # If not reading number yet, note the start
+                if start_index == -1:
+                    start_index = current_index
+            else:
+                # If reading number just finished
+                if start_index != -1:
+                    end_index = current_index - 1
+                    # Check if we should add number to running sum
+                    if is_part(row_index, start_index, end_index):
+                        number_string = lines[row_index][start_index : end_index + 1]
+                        running_sum += int(number_string)
+                    start_index = -1
+                    end_index = -1
+
+            # Increment index
+            current_index += 1
+        # Increment line index
+        row_index += 1
+
+    return running_sum
+
+
+def sum_gear_ratios() -> int:
+    gear_running_sum = 0
+    for value in potential_gears.values():
+        if len(value) == 2:
+            gear_ratio = value[0] * value[1]
+            gear_running_sum += gear_ratio
+    return gear_running_sum
 
 
 # Get contents of file and store as table
@@ -50,36 +107,6 @@ file.close()
 line_length = len(lines[0])
 num_lines = len(lines) - 1  # Empty line
 
-running_sum: int = 0
-row_index: int = 0
-parts = set()
-for line in lines:
-    # Start/end indices are -1 if currently reading a non-digit.
-    current_index: int = 0
-    start_index: int = -1
-    end_index: int = -1
 
-    # Read numbers in line by reading each character
-    for char in line:
-        if char.isdigit():
-            # If not reading number yet, note the start
-            if start_index == -1:
-                start_index = current_index
-        else:
-            # If reading number just finished
-            if start_index != -1:
-                end_index = current_index - 1
-                # Check if we should add number to running sum
-                if is_part(row_index, start_index, end_index):
-                    number_string = lines[row_index][start_index : end_index + 1]
-                    running_sum += int(number_string)
-                    parts.add(int(number_string))
-                start_index = -1
-                end_index = -1
-
-        # Increment index
-        current_index += 1
-    # Increment line index
-    row_index += 1
-
-print(running_sum)
+print("Part number sum:", sum_part_numbers())
+print("Gear ratio sum:", sum_gear_ratios())
