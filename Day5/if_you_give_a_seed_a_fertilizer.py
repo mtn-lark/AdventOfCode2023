37fdaa65b45af31f78652cbfc383d6d766e88992
Lark Mendoza <119717119+mtn-lark@users.noreply.github.com>
2023-12-07 02:17:31 -0800

    Day 5 initial commit
    - example input and input files
    - boilerplate code + docstring

---
+++ Day5/if_you_give_a_seed_a_fertilizer.py
@@ -0,0 +1,13 @@
+"""This script solves Day 5 of Advent of Code 2023.
+
+Part 1:
+Given seeds and a variety of mappings, this script prints the the lowest
+location number that corresponds to any of the initial seeds.
+"""
+
+FILE_PATH = "Day5/input.txt"
+# FILE_PATH = "Day5/example.txt"
+
+with open(FILE_PATH, "r") as file:
+    for line in file:
+        pass
