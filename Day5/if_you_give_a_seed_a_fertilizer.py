10408dfc0566f0e9bc6126bac01aa77bbb31b71b
Lark Mendoza <119717119+mtn-lark@users.noreply.github.com>
2023-12-07 03:12:41 -0800

    Add merge sort for maps

--- Day5/if_you_give_a_seed_a_fertilizer.py
+++ Day5/if_you_give_a_seed_a_fertilizer.py
@@ -7,10 +7,72 @@ location number that corresponds to any of the initial seeds.
 Some docstrings written with help of GitHub copilot.
 """
 
-from typing import List
+from typing import List, Tuple
 
 # FILE_PATH = "Day5/input.txt"
 FILE_PATH = "Day5/example.txt"
+
+
+def merge_map(
+    left: List[Tuple[int, int, int]], right: List[Tuple[int, int, int]]
+) -> List[Tuple[int, int, int]]:
+    """Merges two sorted maps (based on first item of tuple) into one sorted map.
+
+    Args:
+        left (List[Tuple[int, int, int]]): Left sorted map
+        right (List[Tuple[int, int, int]]): Right sorted map
+
+    Returns:
+        List[Tuple[int, int, int]]: Merged sorted map
+    """
+
+    li = 0  # Left pointer index
+    ri = 0  # Right pointer index
+
+    new_list = []
+    while li < len(left) and ri < len(right):
+        if left[li][0] <= right[ri][0]:
+            new_list.append(left[li])
+            li += 1
+        else:
+            # right[ri] < left[li]
+            new_list.append(right[ri])
+            ri += 1
+
+    # If there are any more items in left, merge items
+    while li < len(left):
+        new_list.append(left[li])
+        li += 1
+
+    # If there are any more items in right, merge items
+    while ri < len(right):
+        new_list.append(right[ri])
+        ri += 1
+
+    return new_list
+
+
+def merge_sort_map(map_list: List[Tuple[int, int, int]]) -> List[Tuple[int, int, int]]:
+    """Sorts a mapping list based on the first item of each tuple in the list.
+
+    Args:
+        map_list (List[Tuple[int, int, int]]): List of mappings to be sorted
+
+    Returns:
+        List[Tuple[int, int, int]]: Sorted list of mappings
+    """
+
+    if len(map_list) == 1:
+        return map_list
+
+    mid = (len(map_list)) // 2
+    # Divide and conquer
+    left = merge_sort_map(map_list[0:mid])
+    right = merge_sort_map(map_list[mid : len(map_list)])
+    # Combine lists
+    merge = merge_map(left, right)
+
+    return merge
 
 
 def merge_list(left: List[int], right: List[int]) -> List[int]:
